171d30e6b2b7a67a2fc969c082fe46013890135c
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-22 20:33:57 +0600

    Create poll.js

---
+++ cmds/poll.js
@@ -0,0 +1,21 @@
+  exports.run = async (bot, message, args) => { 
+  let noq = new Discord.RichEmbed()
+  .setColor('#ff0000')
+  .setTitle('Вопрос не найден.')
+  let botmessage = args.join(" ");
+  message.delete().catch();
+  let botreplyi = new Discord.RichEmbed()
+  .setColor('#00ffff')
+  .setTitle(`${botmessage}`)
+  .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
+    if (!args[0]) return message.reply(noq);
+  if (!args) return message.reply("Тебе должны голосовать")
+    const pollTopic = await message.channel.send(botreplyi);
+    pollTopic.react(`✅`);
+    pollTopic.react(`⛔`);
+};
+
+exports.help = {
+    name:"poll"
+
+}
