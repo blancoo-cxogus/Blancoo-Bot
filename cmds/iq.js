0ef1b3056c8a985360cddc4f6230ac7525adff26
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-22 16:25:57 +0600

    Create iq.js

---
+++ cmds/iq.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+function randomIntInc(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+let user = message.mentions.users.first() || message.author;
+let a = randomIntInc(0, 160)
+let embed = new Discord.RichEmbed()
+.addField(":jigsaw: IQ ТЕСТ :jigsaw:", `У ${user.username} ${a} IQ`)
+.setColor("#00ffff")
+.setThumbnail(user.displayAvatarURL)
+
+message.channel.send(embed);}
+
+exports.help = {
+    name:"iq"
+
+}
