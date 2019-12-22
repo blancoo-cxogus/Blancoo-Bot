e15e19fd2da168dc690e50c873153345031e016a
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-22 16:26:37 +0100

    Update dm.js

--- cmds/dm.js
+++ cmds/dm.js
@@ -1,9 +1,12 @@
 const Discord = module.require("discord.js");
 module.exports.run = async (bot,message,args) => {
     let twarn = new Discord.RichEmbed()
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(twarn)
-    .setColor('#ff0000')
-    .setTitle('Недостаточно прав!')
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+        let twarn = new Discord.RichEmbed()
+        .setColor('FF0000')
+        .setDescription('Недостаточно прав!')
+        message.channel.send(twarn)
+    }
     let dmmember = message.mentions.users.first()
     let dmmessage = args.slice(1).join(' ')
     let dmresult = new Discord.RichEmbed()
