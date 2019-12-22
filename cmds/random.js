ce2c05b39ededda8ebaf231bfc739be47a5fd901
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-22 16:28:38 +0600

    Create random.js

---
+++ cmds/random.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+exports.run = (bot, message, args) => 
+{
+    var result = Math.floor(Math.random() * 1000)
+    var random = new Discord.RichEmbed()
+    .setColor('#FFFF00')
+    .setDescription(`${result}`)
+    message.channel.send(random)
+}
+exports.help = {
+    name: 'random-number'
+}
