96c6343b150e19fe2573a787b1a4b9b29a8c9146
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-23 19:40:53 +0600

    Delete nick.js

--- cmds/nick.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-module.exports.run = (bot, message, args) => {
-let nick = args.join(" ")
-message.member.setNickname(`${nick}`)
-module.exports.help = {
-    name: 'nick'
-}
