2c08b95772a9206ede8f14a6252f745ecefe0626
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-23 19:34:42 +0600

    Update nick.js

--- cmds/nick.js
+++ cmds/nick.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
-let user = message.author
 let nick = args.join(" ")
-user.setNickname(nick);
+message.member.setNickname(`${nick}`)
 module.exports.help = {
     name: 'nick'
 }
