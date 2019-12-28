cdfae2fae84e37a1a718608acd424fe9bf01b8ce
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-28 20:39:30 +0600

    Update wink.js

--- cmds/wink.js
+++ cmds/wink.js
@@ -1,1 +1,13 @@
-
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    let buss = message.author
+    message.delete().catch();
+    let lallas = new Discord.RichEmbed()
+     .setColor('#00ffff')
+     .setDescription(`${buss} подмигнул`)
+    message.channel.send(lallas)
+}
+exports.help = {
+    name: "wink"
+}
