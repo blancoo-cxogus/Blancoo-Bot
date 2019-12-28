5d364fbde4ad1cfe9594061fc0cdcbac3a5be5fa
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-28 17:54:26 +0600

    Update smile.js

--- cmds/smile.js
+++ cmds/smile.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot,message,args) => {
     let lalla = new Discord.RichEmbed()
      .setColor('#00ffff')
      .setDescription(`${bus} улыбнулся`)
-     .setThumbnail('https://mpng.pngfly.com/20190517/bhh/kisspng-smiley-emoji-emoticon-portable-network-graphics-cl-emoji-shy-smile-best-hd-wallpaper-5cdf70877f5b18.5174286915581472075217.jpg')
     message.channel.send(lalla)
 }
 exports.help = {
