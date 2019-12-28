af88b391b302085244b7db91970d7ee8325bec3e
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-28 17:21:01 +0600

    Update shoot.js

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -8,15 +8,6 @@ module.exports.run = async (bot,message,args) => {
     let us = message.mentions.users.first()
     let bus = message.author
     message.delete().catch();
-            let yourmamkaschoose =
-    ['Вы умерли!', 'Вы умерли!', 'Вы выжили!'].random()
-
-    let result = new Discord.RichEmbed()
-    .setColor('#fff000')
-    .setTitle('Русская рулетка')
-    .addField('Вы выстрелили в себя и...',`${yourmamkaschoose}`)
-    .setThumbnail('https://cdn.pixabay.com/photo/2017/01/31/23/09/gun-2028034_960_720.png')
-    if(us === bus) return message.reply(result)
     let lalla = new Discord.RichEmbed()
      .setColor('#00ffff')
      .setDescription(`${bus} выстрелил в ${us} ${res}`)
