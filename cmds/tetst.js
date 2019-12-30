ab1b4841cb10338f67cf64b52b6646a7dda92697
TcXogus <59112407+TcXogus@users.noreply.github.com>
2019-12-30 12:59:46 +0600

    Delete tetst.js

--- cmds/tetst.js
+++
@@ -1,8 +0,0 @@
-const Discord = module.require("discord.js");
-const fs = require("fs");
-module.exports.run = async (bot,message,args) => {
-message.channel.send(`<:777:657167461841633296>`)
-};
-module.exports.help = {
-    name: "test"
-}
