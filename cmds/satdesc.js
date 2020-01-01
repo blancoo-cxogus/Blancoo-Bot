3a593cd22f40620493b02a5aa90a3fedb6e88459
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-01 13:51:35 +0600

    Create satdesc.js

---
+++ cmds/satdesc.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(twarn)
+    let twarn = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setTitle('Недостаточно прав!')
+    let tfwarn = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setTitle('Укажи текст сообщения!')
+    let content = args.slice(0).join(' ')
+    let result = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setDescription(`${content}`)
+    if (!content) return message.reply(tfwarn)
+    await message.delete()
+    await message.channel.send(result)
+}
+
+module.exports.help = {
+    name: 'say'
+}
