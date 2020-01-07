70531ef4d690bab20e029c9d5af6ab9126292892
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-07 13:05:44 +0600

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -1,12 +1,18 @@
 exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     const fs = require('fs')
-    let pla = message.author
+    let moder = message.author
     var banRole = message.member.roles.get('663622747238301718')
+     let warnresult = new Discord.RichEmbed()
+        .setColor('#ff0000')
+        .setTitle('Вам был выдан бан!')
+        .addField('От сервера Blancoo',`**Модератор:** ${moder}\n**Причина:** ${warnreason}`)
+        .setThumbnail('http://pngimg.com/uploads/exclamation_mark/exclamation_mark_PNG29.png')
     var embed = new Discord.RichEmbed()
         .setColor('#ff0000')
         .setTitle('Выдан бан!')
         .addField('Подробнее...',`**Модератор:** ${moder}\n**Нарушитель:** ${warnmember}\n**Причина:** ${warnreason}`)
+    warnmember.send(warnresult)
     message.channel.send(embed).then(warnmember.addRole(banRole))
 }
 exports.help = {
