7530ef302a0edc5f0d357df2eb11cf13e6ab64f0
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-07 14:26:30 +0600

    Update report.js

--- cmds/report.js
+++ cmds/report.js
@@ -2,29 +2,29 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let nouser = new Discord.RichEmbed()
   .setColor('#ff0000')
-  .setTitle('Вы не указали пользователя')
+  .setDescription('Вы не указали пользователя')
   let nofinduser = new Discord.RichEmbed()
   .setColor('#ff0000')
-  .setTitle('Не удалось найти пользователя.')
+  .setDescription('Не удалось найти пользователя.')
   let noreason = new Discord.RichEmbed()
   .setColor('#ff0000')
-  .setTitle('Введите причину.')
+  .setDescription('Введите причину.')
    if(!args[0]) return bot.send(nouser);
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.reply(nofinduser);
-    let rreason = args.join(" ").slice(0);
+    let rreason = args.join(" ").slice(1);
     if(!rreason) return message.reply(noreason);
 
     let embed = new Discord.RichEmbed()
     .setColor('#800080')
-    .addField("📕Жалоба на", `${rUser} with ID: ${rUser.id}`)
-    .addField("📝Жалоба от", `${message.author} with ID: ${message.author.id}`)
+    .addField("📕Жалоба на", `${rUser} АЙДИ: ${rUser.id}`)
+    .addField("📝Жалоба от", `${message.author} АЙДИ: ${message.author.id}`)
     .addField("📢Канал", message.channel)
     .addField("📄Причина", rreason)
 
     let okaydm = new Discord.RichEmbed()
-        .setColor('#800080')
-        .setTitle(`Совсем скоро администрация нашего сервера, рассмотрит жалобу на ${rUser.user.tag}`)
+        .setColor('#00ffff')
+        .setDescription(`Совсем скоро администрация нашего сервера, рассмотрит жалобу на ${rUser.user.tag}`)
         message.channel.send(okaydm)
 let rpchannel = message.guild.channels.find('name', '⌈📰⌋┊логи')
 if(!rpchannel) return message.channel.send("Не удалось найти канал для отчетов");
@@ -33,6 +33,5 @@ rpchannel.send(embed)
 }
 
 module.exports.help = {
-  name: "report",
-  aliases: []
+  name: "report"
 }
