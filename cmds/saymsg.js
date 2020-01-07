f6eedb742c98515410604afa55e6907f42701acb
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-07 12:28:25 +0600

    Create saymsg.js

---
+++ cmds/saymsg.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(twarn)
+    let twarn = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setTitle('Недостаточно прав!')
+    let tfwarn = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setTitle('Укажи текст сообщения!')
+    let content = args.slice(0).join(' ')
+    if (!content) return message.reply(tfwarn)
+    await message.delete()
+    await message.channel.send(content)
+}
+
+module.exports.help = {
+    name: 'saymsg'
+}
