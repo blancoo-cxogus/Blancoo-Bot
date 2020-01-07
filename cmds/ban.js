7fe72b9e15aca77340b65e30ab98c33096f900b5
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-07 13:03:35 +0600

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -2,25 +2,13 @@ exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     const fs = require('fs')
     let pla = message.author
-    var caseRole = message.member.roles.get('657889004494258188')
-    function randomIntInc(low, high) {
-        return Math.floor(Math.random() * (high - low + 1) + low);
-    }
-    let nocase = new Discord.RichEmbed()
-    .setColor('#00ffff')
-    .setTitle('У вас нету кейсов для открытия!')
-    if(!caseRole) return message.reply(nocase)
-    let money = randomIntInc(5,20)
-    var replies = [`Вы открыли кейс, и получили ${money}`,`Вы открыли кейс, и получили ${money}`]
-    var result = Math.floor((Math.random() * replies.length));
-    let embed2 = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setColor('#00ffff')
-    .setTitle(`${pla.username} открыл кейс и получил ${money} В-монет`)
+    var banRole = message.member.roles.get('663622747238301718')
     var embed = new Discord.RichEmbed()
-
-    message.channel.send(embed).then(message.member.removeRole(caseRole))
+        .setColor('#ff0000')
+        .setTitle('Выдан бан!')
+        .addField('Подробнее...',`**Модератор:** ${moder}\n**Нарушитель:** ${warnmember}\n**Причина:** ${warnreason}`)
+    message.channel.send(embed).then(warnmember.addRole(banRole))
 }
 exports.help = {
-    name: 'open-case'
+    name: 'ban'
 }
