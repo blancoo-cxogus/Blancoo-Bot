9ead4746aa9cd5fda57df68eafc2867e3aff5759
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-08 16:44:57 +0600

    Update calc.js

--- cmds/calc.js
+++ cmds/calc.js
@@ -2,8 +2,10 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
     let ddos = args.join(" ")
     let onef = Math.round(ddos)
-    if (err) return message.reply("Ошибка")
-    message.channel.send(onef) 
+   catch(err){
+    console.log(err.name)
+    message.reply("произошла ошибка!")
+}
 }
 module.exports.help = {
     name: "calc"
