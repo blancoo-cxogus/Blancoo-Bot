c2793e6c9fd94a4d969fc73aa9c69d71daceac4b
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-08 16:50:07 +0600

    Delete calc.js

--- cmds/calc.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-module.exports.run = (bot, message, args) => {
-    let ddos = args.join(" ")
-    let onef = Math.round(ddos)
-}
-module.exports.help = {
-    name: "calc"
-};
