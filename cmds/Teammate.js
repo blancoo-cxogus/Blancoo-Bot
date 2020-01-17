12e8ec40eec2730dde3e8fe83055c3ccb744c949
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-17 18:51:11 +0600

    Create Teammate.js

---
+++ cmds/Teammate.js
@@ -0,0 +1,25 @@
+  
+const Discord = require('discord.js')
+
+exports.run = async (bot, message, args) => { 
+  let noq = new Discord.RichEmbed()
+  .setColor('#ff0000')
+  .setTitle('Использование: ,teammate [Название игры]')
+  let player = message.author
+  let game = args.join(" ");
+  message.delete().catch();
+  let botreplyi = new Discord.RichEmbed()
+  .setColor('#00ffff')
+  .setTitle(`🎮 ${player} ищет тиммейта в игру ${game}!`)
+  .setDescription('Если ты согласен, напиши ему в личные сообщения.')
+  .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
+  let ideachannel = message.guild.channels.find('name', '⌈🎮⌋┊тиммейты')
+if(!ideachannel) return message.channel.send("Не удалось найти канал для отчетов");
+    if (!args[0]) return message.reply(noq);
+    ideachannel.send(botreplyi)
+};
+
+exports.help = {
+    name:"teammate"
+
+}
