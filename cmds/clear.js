f2c838e9eecf2c4fb799315d38dae279fc927240
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-17 23:43:41 +0600

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -1,26 +1,13 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    let erro = new Discord.RichEmbed()
-     .setColor('#00ffff')
-     .setTitle('У вас нет прав!')
-    let errs = new Discord.RichEmbed()
-     .setColor('#00ffff')
-     .setTitle('Укажите значение меньше 100')
-    let errt = new Discord.RichEmbed()
-     .setColor('#00ffff')
-     .setTitle('Укажите значение больше 1')
-    let result = new Discord.RichEmbed()
-     .setColor('#00ffff')
-     .setTitle(`Удалено ${args[0]} сообщений`)
     try{
-    if(!message.member.hasPermission("MANAGE_MESSAGES"))return message.channel.send(erro);
-    if(args[0]>100) return bot.send(errs);
-    if(args[0]<1) return bot.send(errt);
+    if(!message.member.hasPermission("MANAGE_MESSAGES"))return message.channel.send("<:xmark:651075973378211847> У вас нет прав!");
+    if(args[0]>100) return bot.send("<:xmark:651075973378211847> Укажите значение меньше 100");
+    if(args[0]<1) return bot.send("<:xmark:651075973378211847> Укажите значение больше 1");
     message.channel.bulkDelete(args[0]).then(() =>{
-        message.channel.send(result).then(msg => msg.delete(15*1000));
+        message.channel.send(`<a:YesGif:651075988490551307> Удалено ${args[0]} сообщений.`).then(msg => msg.delete(15*1000));
     });
-    bot.send(botmessage);
 }catch(err){
     console.log(err.name)
 }
