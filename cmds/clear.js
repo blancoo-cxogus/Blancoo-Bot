d26121028cb715085be5d2c9fd5d0cca7423097a
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-18 00:27:23 +0600

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     try{
     if(!message.member.hasPermission("MANAGE_MESSAGES"))return message.channel.send("<:xmark:651075973378211847> У вас нет прав!");
-    if(!args[0]) returm bot.send("<:xmark:651075973378211847> Укажите значение")
+    if(!args[0]) return bot.send("<:xmark:651075973378211847> Укажите значение")
     if(args[0]>100) return bot.send("<:xmark:651075973378211847> Укажите значение меньше 100");
     if(args[0]<1) return bot.send("<:xmark:651075973378211847> Укажите значение больше 1");
     message.channel.bulkDelete(args[0]).then(() =>{
