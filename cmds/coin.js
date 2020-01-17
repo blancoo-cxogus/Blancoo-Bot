74e95bad2bc49d011e311aab27160de27e409c64
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-17 23:46:28 +0600

    Update coin.js

--- cmds/coin.js
+++ cmds/coin.js
@@ -3,14 +3,9 @@ Array.prototype.random = function() {
     return this[Math.floor(Math.random() * this.length)]
 }
 module.exports.run = async (bot, message, args) => {
-    let noq = new Discord.RichEmbed()
-    .setColor('#ff0000')
-    .setTitle('Использование: ,coin <Решка/Орел/Ребро>.')
     let think = args.join(" ")
     let choose =
     ['Ребро', 'Орел', 'Решка'].random()
-    if (!args[0]) return message.reply(noq);
-
     let result = new Discord.RichEmbed()
     .setColor('#fff000')
     .setTitle('Монетка')
@@ -18,11 +13,10 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail('http://pngimg.com/uploads/coin/coin_PNG36943.png')
     if(think === choose) 
     result.setDescription(`Вам выпал(а) ${choose} \n **Удача!**`)
-
-
+    if(!think)
+    result.setDescription(`Вам выпал(а) ${choose}`)
     message.channel.send(result)
 }
-
 module.exports.help = {
     name: 'coin'
 }
