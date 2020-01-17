9906a3eefc4cdd09883aa2360b6b417c5f17e559
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-17 23:29:05 +0600

    Update When.js

--- cmds/When.js
+++ cmds/When.js
@@ -1,18 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
-    if (!args[0]) return message.reply(fwarn);
-    let fwarn = new Discord.RichEmbed()
-    .setColor('#ff0000')
-    .setTitle('Пожайлуста, напишите вопрос')
+    if (!args[0]) return message.reply("<:xmark:651075973378211847> Вопрос не найден");
     let replies = ["Прямо сейчас", "Завтра", "Вчера", "Сейчас не могу дать точного ответа", "Что за тупые вопросы?","Никогда","Скоро","Тебе еще очень долго ждать", "Через месяц", "Через год", "Жди неделю..."];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(0).join(" ");
-    if(!question.includes('?')) return message.reply(swarn)
-    let swarn = new Discord.RichEmbed()
-    .setColor('#ff0000')
-    .setTitle('Это не вопрос, поставь вопросительный знак.')
-  
+    if(!question.includes('?')) return message.reply("<:xmark:651075973378211847> Это не вопрос, поставь вопросительный знак")
     if(question === "Когда будет третья мировая?") replies[result] = 'Я планирую ее сделать скоро.\n **ПАСХАЛКА**'
     if(question === "Когда Blancoo удалят?") replies[result] = 'Надеюсь, что никогда\n **ПАСХАЛКА**'
     if(question === "Когда тебя удалят?") replies[result] = 'Пожайлуста, не надо меня удалять. Я очень хороший бот и слушаюсь хозяина. :(\n **ПАСХАЛКА**'
