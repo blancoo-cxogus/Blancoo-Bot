919889b5a8743cf103d7b005ff73f1ca4c9b8ba7
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-17 23:56:03 +0600

    Update neweightball.js

--- cmds/neweightball.js
+++ cmds/neweightball.js
@@ -1,18 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args) => {
-    if (!args[0]) return message.reply(fwarn);
-    let fwarn = new Discord.RichEmbed()
-    .setColor('#ff0000')
-    .setTitle('Пожайлуста, напишите вопрос')
+    if (!args[0]) return bot.send("<:xmark:651075973378211847> Пожайлуста, напишите вопрос.");
     let replies = ["Да", "Нет", "Ну, не знаю", "Сейчас не могу дать точного ответа", "Что за тупые вопросы?","Возможно","Скорее нет","Не совсем"];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(0).join(" ");
-    if(!question.includes('?')) return message.reply(swarn)
-    let swarn = new Discord.RichEmbed()
-    .setColor('#ff0000')
-    .setTitle('Это не вопрос, поставь вопросительный знак.')
+    if(!question.includes('?')) return bot.send("<:xmark:651075973378211847> Это не вопрос, поставь вопросительный знак")
   
     if(question === "У меня есть смысл жить?") replies[result] = 'Нет, умри нахуй\n **ПАСХАЛКА**'
     if(question === "Где смотреть аниме?") replies[result] = 'Только на youmiteru.ru!\n **ПАСХАЛКА**' 
