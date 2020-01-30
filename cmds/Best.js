e75d8acec51f003eaa9763e7fc92eef0b285192a
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-01-30 23:04:11 +0600

    Update Best.js

--- cmds/Best.js
+++ cmds/Best.js
@@ -2,14 +2,15 @@ const Discord = module.require("discord.js");
 module.exports.run = async (bot,message,args) => {
     if (!message.member.hasPermission("ADMINISTRATOR")) return bot.send('<:xmark:651075973378211847> У вас нету прав!')
     let user = message.mentions.users.first()
-    let user = args.slice(1).join(' ')
+    let usern = args.slice(1).join(' ')
     let dmresult = new Discord.RichEmbed()
-        .setColor('#00ffff')
-        .setTitle('user')
-        .setDescription(`${dmmessage}`)
-    dmmember.send(dmresult)
+      .setColor('#00ffff')
+      .setTitle(user.username)
+      .setDescription(usern)
+      .setThumbnail(`${user.displayAvatarURL}`)
+    message.channel.send(dmresult)
     message.delete()
 };
 module.exports.help = {
-    name: "dm"
+    name: "best"
 }
