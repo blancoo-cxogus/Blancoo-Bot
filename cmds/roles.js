bb0be66918ef0a27bb031ed66545ebf7b26f28c4
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-02-08 16:11:08 +0600

    Create roles.js

---
+++ cmds/roles.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("ADMINISTATOR")) return message.reply(twarn)
+    let twarn = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setTitle('Недостаточно прав!')
+    let tfwarn = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setTitle('Укажи текст сообщения!')
+    let content = args.slice(0).join(' ')
+    let result = new Discord.RichEmbed()
+    .setColor('#00ffff')
+    .setTitle('Навигация по серверу')
+    .setDescription(content)
+    .setThumbnail('https://img1.freepng.ru/20180421/hqq/kisspng-airplane-paper-plane-computer-icons-paper-airplane-5adbfef2636298.7674239715243670904071.jpg')
+    if (!content) return message.reply(tfwarn)
+    await message.delete()
+    await message.channel.send(result)
+}
+
+module.exports.help = {
+    name: 'inf2roles3247'
+}
