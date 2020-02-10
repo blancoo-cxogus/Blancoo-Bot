98469b18dfc2649635c9b411321d09a73c07abff
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-02-10 23:12:05 +0600

    Create spinner.js

---
+++ cmds/spinner.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+function randomIntInc(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+let user = message.author;
+let a = randomIntInc(1, 59)
+let b = randomIntInc(0, 2)
+let embed = new Discord.RichEmbed()
+.setTitle('Спиннер')
+.setDescription(Ваш спиннер прокрутился  ${b} минут ${a} секунд.)
+.setColor("#00ffff")
+.setThumbnail(user.displayAvatarURL)
+
+message.channel.send(embed);}
+
+exports.help = {
+    name:"spinner"
+
+}
