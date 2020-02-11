3d35a2062688cf8b69af594d6f55efda1fa88691
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-02-11 16:35:31 +0600

    Create webhook.js

---
+++ cmds/webhook.js
@@ -0,0 +1,7 @@
+const Discord = module.require("discord.js");
+module.exports.run = async (bot,message,args) => {
+    message.channel.send(".", [username = 'help', avatarURL = 'https://lh3.googleusercontent.com/proxy/VOUrNvO2N_9PXHBc4SqLT21agK1UzYnq5nbtpvx0fJQRE4S1QScYytFZOZuwZgam4bToviduXB--RXBXQgmwgocjgtRec-csOgv_6AMxQv38Dzb5XC67hdIALA'])
+}
+exports.help = {
+    name: "wbh"
+}
