385423613b8d82e171e89c8ceba5ee630aa3e90b
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-02-11 17:02:20 +0600

    Delete webhook.js

--- cmds/webhook.js
+++
@@ -1,8 +0,0 @@
-const Discord = module.require("discord.js");
-module.exports.run = async (bot,message,args) => {
-   let webhook = message.channel.createWebhook('Webhook', 'https://lh3.googleusercontent.com/proxy/VOUrNvO2N_9PXHBc4SqLT21agK1UzYnq5nbtpvx0fJQRE4S1QScYytFZOZuwZgam4bToviduXB--RXBXQgmwgocjgtRec-csOgv_6AMxQv38Dzb5XC67hdIALA')
-   webhook.send('test')
-}
-exports.help = {
-    name: "wbh"
-}
