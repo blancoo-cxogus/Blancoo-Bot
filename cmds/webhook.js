075ece8998e5b17dd9048d45a046b6bc88573dcd
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-02-11 16:43:26 +0600

    Update webhook.js

--- cmds/webhook.js
+++ cmds/webhook.js
@@ -1,8 +1,6 @@
 const Discord = module.require("discord.js");
 module.exports.run = async (bot,message,args) => {
-    let webhook = new Discord.webhook()
-      .username("Тест")
-      .avatarURL('https://lh3.googleusercontent.com/proxy/VOUrNvO2N_9PXHBc4SqLT21agK1UzYnq5nbtpvx0fJQRE4S1QScYytFZOZuwZgam4bToviduXB--RXBXQgmwgocjgtRec-csOgv_6AMxQv38Dzb5XC67hdIALA')
+   let webhook = bot.createWebhook('Snek', 'https://lh3.googleusercontent.com/proxy/VOUrNvO2N_9PXHBc4SqLT21agK1UzYnq5nbtpvx0fJQRE4S1QScYytFZOZuwZgam4bToviduXB--RXBXQgmwgocjgtRec-csOgv_6AMxQv38Dzb5XC67hdIALA')
     webhook.send('Hello!')
 }
 exports.help = {
