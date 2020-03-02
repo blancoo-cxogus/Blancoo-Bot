c2b57c1fa7b473afab97daa9fa26ded08e284e54
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-03-02 18:16:05 +0600

    Update hook.js

--- cmds/hook.js
+++ cmds/hook.js
@@ -1,12 +1,12 @@
 const discord = require("discord.js");
 const webhook = require("webhook-discord");
 module.exports.run = async (bot, message, args) => {
+let messagehook = args.slice(0).join(' ')
 const Hook = new webhook.Webhook("https://discordapp.com/api/webhooks/684008202635051052/kjFunNILxRPZ42b0-U-LPtww681g5wBirgq0PnpTYIWemmR9EMRTRslCFPH5fdpDpzXv"); // Прямая ссылка на вЕбхук
- 
+let user = message.author
 const msg = new webhook.MessageBuilder()
-                .setName("Я крутой") // Название вебхука, если нету то по дефолту стоит
-                .setText("Класс") // Текст вне embed текста, или внутри, я не помню.
-                .setTime();
+                .setName(user.username) // Название вебхука, если нету то по дефолту стоит
+                .setText(messagehook) // Текст вне embed текста, или внутри, я не помню.
  
 Hook.send(msg);} // Отправить вебхук
 module.exports.help = {
