b76920a9e12f65006aac1e1d5542079c7ac2d43c
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-03-02 18:40:41 +0600

    Update hook.js

--- cmds/hook.js
+++ cmds/hook.js
@@ -1,13 +1,14 @@
 const discord = require("discord.js");
 const webhook = require("webhook-discord");
 module.exports.run = async (bot, message, args) => {
+if (!message.member.hasPermission("KICK_MEMBERS")) return bot.send('<:xmark:651075973378211847> У вас нету прав!')
 let messagehook = args.slice(0).join(' ')
 const Hook = new webhook.Webhook("https://discordapp.com/api/webhooks/684008202635051052/kjFunNILxRPZ42b0-U-LPtww681g5wBirgq0PnpTYIWemmR9EMRTRslCFPH5fdpDpzXv"); // Прямая ссылка на вЕбхук
 let user = message.author
 const msg = new webhook.MessageBuilder()
                 .setName("Blancoo")
                 .setColor("#00ffff")
-                .addField("Сообщение", `${messagehook}`)
+                .addField(`Сообщение от ${user.username}`, `${messagehook}`)
  
 Hook.send(msg);} 
 module.exports.help = {
