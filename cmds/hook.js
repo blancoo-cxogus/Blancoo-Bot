91707373a131f5384169febecd0207b883a64d9f
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-03-02 18:36:18 +0600

    Update hook.js

--- cmds/hook.js
+++ cmds/hook.js
@@ -5,8 +5,9 @@ let messagehook = args.slice(0).join(' ')
 const Hook = new webhook.Webhook("https://discordapp.com/api/webhooks/684008202635051052/kjFunNILxRPZ42b0-U-LPtww681g5wBirgq0PnpTYIWemmR9EMRTRslCFPH5fdpDpzXv"); // Прямая ссылка на вЕбхук
 let user = message.author
 const msg = new webhook.MessageBuilder()
-                .setName(user.username)
-                .setText(messagehook)
+                .setName("Blancoo")
+                .setColor("#00ffff")
+                .addField("Сообщение", `${messagehook}`)
  
 Hook.send(msg);} 
 module.exports.help = {
