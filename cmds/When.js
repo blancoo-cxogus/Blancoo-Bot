4bbaf65e77aeb7fd6c22a9406e818d4a57b6568d
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-06-14 12:37:48 +0600

    Update When.js

--- cmds/When.js
+++ cmds/When.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args) => {
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(0).join(" ");
-    if(!question.includes('?')) return bot.send("<:xmark:651075973378211847> Это не вопрос, поставь вопросительный знак")
+    if(!question.includes('?')) return bot.send("Это не вопрос, поставь вопросительный знак")
     if(question === "Когда будет третья мировая?") replies[result] = 'Я планирую ее сделать скоро.\n **ПАСХАЛКА**'
     if(question === "Когда Blancoo удалят?") replies[result] = 'Надеюсь, что никогда\n **ПАСХАЛКА**'
     if(question === "Когда тебя удалят?") replies[result] = 'Пожайлуста, не надо меня удалять. Я очень хороший бот и слушаюсь хозяина. :(\n **ПАСХАЛКА**'
