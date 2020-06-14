f15486f18a7e76601628b6ad307471bdf671b3d2
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-06-14 13:14:44 +0600

    Update penis.js

--- cmds/penis.js
+++ cmds/penis.js
@@ -1,11 +1,11 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
 function penis(size) {
 return "8" + "=".repeat(size) + "D"
 }
 function randomIntInc(low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
 }
-const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
 let b = randomIntInc(2, 20)
 let a = penis(b)
 message.channel.send(a)}
