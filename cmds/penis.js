905ddb74318a533d56efa7aa112d8634a3f54823
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-06-14 13:07:48 +0600

    Update penis.js

--- cmds/penis.js
+++ cmds/penis.js
@@ -3,6 +3,7 @@ return "8" + "=".repeat(size) + "D"
 }
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+let b = args.join(" ")
 let a = penis(b)
 message.channel.send(a)}
 
