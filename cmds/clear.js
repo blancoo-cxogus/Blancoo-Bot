860b3e63b5794f61c0be11aaf32475d302743bd1
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-06-23 16:05:25 +0600

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot,message,args) => {
     if(args[0]>100) return bot.send("<:xmark:651075973378211847> Укажите значение меньше 100");
     if(args[0]<1) return bot.send("<:xmark:651075973378211847> Укажите значение больше 1");
     message.channel.bulkDelete(args[0]).then(() =>{
-        message.channel.send(`<a:YesGif:651075988490551307> Удалено ${args[0]} сообщений.`).then(msg => msg.delete(15*1000));
+        message.channel.send(`Удалено ${args[0]} сообщений.`).then(msg => msg.delete(15*1000));
     });
 }catch(err){
     console.log(err.name)
