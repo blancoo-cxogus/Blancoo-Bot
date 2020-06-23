28e6cfc70d3a752246d5a8bbc52f60c7d7b6de7f
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-06-23 16:28:21 +0600

    Update saymsg.js

--- cmds/saymsg.js
+++ cmds/saymsg.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(twarn)
     let twarn = new Discord.RichEmbed()
-    .setColor('#00ffff')
+    .setColor('#00ff14')
     .setTitle('Недостаточно прав!')
     let tfwarn = new Discord.RichEmbed()
-    .setColor('#00ffff')
+    .setColor('#00ff14')
     .setTitle('Укажи текст сообщения!')
     let content = args.slice(0).join(' ')
     if (!content) return message.reply(tfwarn)
