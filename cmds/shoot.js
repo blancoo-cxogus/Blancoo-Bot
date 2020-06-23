ecd6b330349f7dd4f0bce2767c8d1d2a4d93afa8
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-06-23 16:29:46 +0600

    Update shoot.js

--- cmds/shoot.js
+++ cmds/shoot.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot,message,args) => {
     if(us === bus) return bot.send(result)
     message.delete().catch();
     let lalla = new Discord.RichEmbed()
-     .setColor('#00ffff')
+     .setColor('#00ff14')
      .setDescription(`${bus} выстрелил в ${us} ${res}`)
      .setThumbnail('https://cdn.pixabay.com/photo/2017/01/31/23/09/gun-2028034_960_720.png')
     if(!us) return bot.send('<:xmark:651075973378211847> Пожайлуста, укажите пользователя')
