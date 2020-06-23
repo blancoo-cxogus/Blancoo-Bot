21db25181b503f9be6c4c43e0d221d831fdc35c6
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-06-23 16:08:18 +0600

    Update lovecalculator.js

--- cmds/lovecalculator.js
+++ cmds/lovecalculator.js
@@ -9,7 +9,7 @@ let c = randomIntInc(0, 100)
 let embed = new Discord.RichEmbed()
 .setTitle('Калькулятор любви')
 .setDescription(`${a} любит ${b} на ${c}%`)
-.setColor("#00ffff")
+.setColor("#00ff14")
 .setThumbnail('https://pngimg.com/uploads/heart/heart_PNG51337.png')
 if(c === 0) {
   embed.setDescription(`${a} не любит ${b}`)
