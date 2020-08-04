0c8e26f57ec17251cc11b2682c9987e7d12e27d8
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-08-04 11:31:01 +0600

    Create stop.js

---
+++ cmds/stop.js
@@ -0,0 +1,22 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+        Array.prototype.random = function() {
+        return this[Math.floor(Math.random() * this.length)]
+    }
+    let ress = [`но он увернулся`,`и он не успел увернутся`].random()
+    let uss = message.mentions.users.first()
+    let buss = message.author
+    message.delete().catch();
+    let lallas = new Discord.RichEmbed()
+     .setColor('#00ff14')
+     .setDescription(`${buss} хочет что-то сказать!`)
+    if(!uss) return bot.send('<:xmark:651075973378211847> Пожайлуста, укажите пользователя')
+    if(uss === buss) {
+        lallas.setDescription(`${buss} укусил самого себя`)
+    }
+    message.channel.send(lallas)
+}
+exports.help = {
+    name: "stop"
+}
