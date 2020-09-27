b6b2a95edf31baa703ffa75f83cedf2699fa387f
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-09-27 13:15:25 +0600

    Create BSAY.js

---
+++ cmds/BSAY.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(twarn)
+    let twarn = new Discord.RichEmbed()
+    .setColor('#00ff14')
+    .setTitle('Недостаточно прав!')
+    let tfwarn = new Discord.RichEmbed()
+    .setColor('#00ff14')
+    .setTitle('Укажи текст сообщения!')
+    let content = args.slice(0).join(' ')
+    let result = new Discord.RichEmbed()
+    .setColor('#000000')
+    .setDescription(`${content}`)
+    if (!content) return message.reply(tfwarn)
+    await message.delete()
+    await message.channel.send(result)
+}
+
+module.exports.help = {
+    name: 'bsay'
+}
