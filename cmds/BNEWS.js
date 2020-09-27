50f8e775143f78f795ebc22d652fc0f7a5c0a5b2
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-09-27 13:15:45 +0600

    Update BNEWS.js

--- cmds/BNEWS.js
+++ cmds/BNEWS.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
     let content = args.slice(0).join(' ')
     let result = new Discord.RichEmbed()
     .setColor('#000000')
-    .setTitle(':newspaper2: Новости Blancoo)
+    .setTitle(':newspaper2: Новости Blancoo')
     .setDescription(content)
     .setThumbnail('https://cdn.discordapp.com/attachments/759674163001557013/759674202436796446/iYSC2BYZ7.jpg')
     if (!content) return message.reply(tfwarn)
