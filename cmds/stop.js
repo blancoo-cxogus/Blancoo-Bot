5555ed740901fc8707a0a00db29fb54d8cfa465d
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-09-27 13:33:08 +0600

    Update stop.js

--- cmds/stop.js
+++ cmds/stop.js
@@ -1,16 +1,23 @@
-const Discord = module.require("discord.js");
-const fs = require("fs");
-module.exports.run = async (bot,message,args) => {
-        Array.prototype.random = function() {
-        return this[Math.floor(Math.random() * this.length)]
-    }
-    let buss = message.author
-    message.delete().catch();
-    let lallas = new Discord.RichEmbed()
-     .setColor('#00ff14')
-     .setDescription(`${buss} хочет что-то сказать!`)
-    message.channel.send(lallas)
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("ADMINISTATOR")) return message.reply(twarn)
+    let twarn = new Discord.RichEmbed()
+    .setColor('#00ff14')
+    .setTitle('Недостаточно прав!')
+    let tfwarn = new Discord.RichEmbed()
+    .setColor('#00ff14')
+    .setTitle('Укажи текст сообщения!')
+    let content = args.slice(0).join(' ')
+    let result = new Discord.RichEmbed()
+    .setColor('#000000')
+    .setTitle(':newspaper2: Новости Blancoo')
+    .setDescription(content)
+    .setThumbnail('https://cdn.discordapp.com/attachments/759674163001557013/759674202436796446/iYSC2BYZ7.jpg')
+    if (!content) return message.reply(tfwarn)
+    await message.delete()
+    await message.channel.send(result)
 }
-exports.help = {
-    name: "stop"
+
+module.exports.help = {
+    name: 'bnews'
 }
