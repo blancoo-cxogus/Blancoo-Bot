0f06238ba0fa1f98baea3c13874cd25e2563eccc
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-02 16:41:22 +0600

    Update spinner.js

--- cmds/spinner.js
+++ cmds/spinner.js
@@ -9,7 +9,7 @@ let b = randomIntInc(0, 2)
 let embed = new Discord.RichEmbed()
 .setTitle('Спиннер')
 .setDescription(`Ваш спиннер прокрутился  ${b} минут ${a} секунд.`)
-.setColor("#00ff14")
+.setColor("#ffffff")
 .setThumbnail(user.displayAvatarURL)
 
 message.channel.send(embed);}
