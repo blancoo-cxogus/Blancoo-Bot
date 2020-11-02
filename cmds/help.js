43b0bb2e689305055f02a7aea37602e97c65cb0c
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-02 16:14:49 +0600

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -6,12 +6,12 @@ module.exports.run = async (bot,message,args) => {
     .addField('——————————————————————————————————','``,avatar [@человек]`` - посмотреть аватарку человека\n``,tableflip`` - анимированный переворот стола\n``,ping`` - чекнуть пинг юзера\n``,iq [@человек]`` - проверить iq человека')
     .addField('——————————————————————————————————','``,8ball [вопрос]``- гадалка\n``,coin <Решка/Орел/Ребро>`` - орел, решка и ребро\n``,rr`` или ``,russian-roulette`` - русская рулетка\n``,cube`` - бросить кубик')
     .addField('——————————————————————————————————','``,random-number`` - бот отправляет случайное число\n``,random-color`` - отправляет случайный цвет, и его hex код\n')
-    .setColor("#00ff14")
+    .setColor('#ffffff')
     
     message.member.send(embed);
 let embed1 = new Discord.RichEmbed()
 .setTitle('Меню помощи было выслано вам в лс.')
-.setColor('#00ffff')
+.setColor('#ffffff')
 message.channel.send(embed1)
 };
 module.exports.help = {
