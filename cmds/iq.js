1903dba25b66521671e62e2f6a26440e4edf157f
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-02 16:36:14 +0600

    Update iq.js

--- cmds/iq.js
+++ cmds/iq.js
@@ -4,7 +4,7 @@ function randomIntInc(low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
 }
 let user = message.mentions.users.first() || message.author;
-let a = randomIntInc(1, 164)
+let a = randomIntInc(1, 179)
 let embed = new Discord.RichEmbed()
 .setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} ${a} IQ`)
 .setColor("#ffffff")
@@ -13,12 +13,57 @@ let embed = new Discord.RichEmbed()
    embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nОшибка! "-999" это не число!`)
  }
  if(a === 162){
-   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ больше, чем ожидалось.`)
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nОшибка! "-999" это не число!`)
  }
  if(a === 163){
-   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ меньше, чем ожидалось.`)
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nОшибка! "-999" это не число!`)
  }
  if(a === 164){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nОшибка! "-999" это не число!`)
+ }
+ if(a === 165){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ больше, чем ожидалось.`)
+ }
+ if(a === 166){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ больше, чем ожидалось.`)
+ }
+ if(a === 167){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ больше, чем ожидалось.`)
+ }
+ if(a === 168){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ больше, чем ожидалось.`)
+ }
+ if(a === 169){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ меньше, чем ожидалось.`)
+ }
+ if(a === 170){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ меньше, чем ожидалось.`)
+ }
+ if(a === 171){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ меньше, чем ожидалось.`)
+ }
+ if(a === 172){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ меньше, чем ожидалось.`)
+ }
+ if(a === 173){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ меньше, чем ожидалось.`)
+ }
+ if(a === 174){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nНе удалось определить IQ пользователя ${user.username}`)
+ }
+ if(a === 175){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nНе удалось определить IQ пользователя ${user.username}`)
+ }
+ if(a === 176){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nНе удалось определить IQ пользователя ${user.username}`)
+ }
+ if(a === 177){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nНе удалось определить IQ пользователя ${user.username}`)
+ }
+ if(a === 178){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nНе удалось определить IQ пользователя ${user.username}`)
+ }
+ if(a === 179){
    embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nНе удалось определить IQ пользователя ${user.username}`)
  }
 message.channel.send(embed);}
