7499b02b1c5d824935fc951720b43d508f4d7e12
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-02 16:17:56 +0600

    Update cube.js

--- cmds/cube.js
+++ cmds/cube.js
@@ -7,7 +7,7 @@ let user =  message.author;
 let a = randomIntInc(1, 6)
 let embed = new Discord.RichEmbed()
 .setDescription(`${user.username} бросил кубик и он показал **${a}**`)
-.setColor("#00ff14")
+.setColor("#ffffff")
 .setThumbnail('http://pngimg.com/uploads/dice/dice_PNG110.png')
 message.channel.send(embed);}
 
