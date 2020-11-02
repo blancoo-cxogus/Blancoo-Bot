df835e166217b00a8c4401d94412618eb773d447
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-02 16:24:38 +0600

    Update iq.js

--- cmds/iq.js
+++ cmds/iq.js
@@ -4,13 +4,23 @@ function randomIntInc(low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
 }
 let user = message.mentions.users.first() || message.author;
-let a = randomIntInc(0, 161)
+let a = randomIntInc(1, 164)
 let embed = new Discord.RichEmbed()
-.addField(":jigsaw: IQ ТЕСТ :jigsaw:", `У ${user.username} ${a} IQ`)
-.setColor("#00ff14")
+.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} ${a} IQ`)
+.setColor("#ffffff")
 .setThumbnail(user.displayAvatarURL)
- if(a==161)
-
+ if(a === 161){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nОшибка! "-999" это не число!`)
+ }
+ if(a === 162){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ больше, чем ожидалось.`)
+ }
+ if(a === 163){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nУ ${user.username} IQ меньше, чем ожидалось.`)
+ }
+ if(a === 164){
+   embed.setDescription(`:jigsaw: IQ ТЕСТ :jigsaw:\nНе удалось определить IQ пользователя ${user.username}`)
+ }
 message.channel.send(embed);}
 
 exports.help = {
