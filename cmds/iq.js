a0b7e4cff5ed9828b981300ac9aaab4d15a64c9c
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-02 16:20:14 +0600

    Update iq.js

--- cmds/iq.js
+++ cmds/iq.js
@@ -4,11 +4,12 @@ function randomIntInc(low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
 }
 let user = message.mentions.users.first() || message.author;
-let a = randomIntInc(0, 160)
+let a = randomIntInc(0, 161)
 let embed = new Discord.RichEmbed()
 .addField(":jigsaw: IQ ТЕСТ :jigsaw:", `У ${user.username} ${a} IQ`)
 .setColor("#00ff14")
 .setThumbnail(user.displayAvatarURL)
+ if(a==161)
 
 message.channel.send(embed);}
 
