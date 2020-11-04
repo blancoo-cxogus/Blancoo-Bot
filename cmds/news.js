00d586ecb546d34f8737c29b87937a036c1cbc45
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-04 14:42:28 +0600

    Update news.js

--- cmds/news.js
+++ cmds/news.js
@@ -2,9 +2,13 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("ADMINISTATOR")) return message.reply(twarn)
     let twarn = new Discord.RichEmbed()
+    .setAuthor(`Ошибка`,'https://images-ext-2.discordapp.net/external/9Q_b70cs1yvSzxGzU0wXo2QWUDMr12uRi5FUIybQcNQ/%3Fv%3D1/https/cdn.discordapp.com/emojis/695541716760920086.gif')   
+    .setTimestamp()
     .setColor('#00ff14')
     .setTitle('Недостаточно прав!')
     let tfwarn = new Discord.RichEmbed()
+    .setAuthor(`Ошибка`,'https://images-ext-2.discordapp.net/external/9Q_b70cs1yvSzxGzU0wXo2QWUDMr12uRi5FUIybQcNQ/%3Fv%3D1/https/cdn.discordapp.com/emojis/695541716760920086.gif')   
+    .setTimestamp()
     .setColor('#00ff14')
     .setTitle('Укажи текст сообщения!')
     let content = args.slice(0).join(' ')
