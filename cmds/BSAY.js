fbe345c0744f1b18be4cea8a62b557eb18552560
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-04 14:34:15 +0600

    Update BSAY.js

--- cmds/BSAY.js
+++ cmds/BSAY.js
@@ -2,9 +2,13 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(twarn)
     let twarn = new Discord.RichEmbed()
+    .setAuthor(`Пасхалка`,'https://media.discordapp.net/attachments/695277643360239616/745584175373090886/hypeshiny.gif')   
+    .setTimestamp()
     .setColor('#00ff14')
     .setTitle('Недостаточно прав!')
     let tfwarn = new Discord.RichEmbed()
+    .setAuthor(`Пасхалка`,'https://media.discordapp.net/attachments/695277643360239616/745584175373090886/hypeshiny.gif')   
+    .setTimestamp()
     .setColor('#00ff14')
     .setTitle('Укажи текст сообщения!')
     let content = args.slice(0).join(' ')
