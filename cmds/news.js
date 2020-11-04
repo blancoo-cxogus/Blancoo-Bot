501ea8972b1bea80ecc0ca1fa273cd40f8abb008
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-04 14:49:37 +0600

    Update news.js

--- cmds/news.js
+++ cmds/news.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("ADMINISTATOR")) return message.reply(twarn)
     let twarn = new Discord.RichEmbed()
     .setAuthor(`Ошибка`,'https://images-ext-2.discordapp.net/external/9Q_b70cs1yvSzxGzU0wXo2QWUDMr12uRi5FUIybQcNQ/%3Fv%3D1/https/cdn.discordapp.com/emojis/695541716760920086.gif')   
     .setTimestamp()
     .setColor('#00ff14')
     .setTitle('Недостаточно прав!')
+    if (!message.member.hasPermission("ADMINISTATOR")) return message.reply(twarn)
     let tfwarn = new Discord.RichEmbed()
     .setAuthor(`Ошибка`,'https://images-ext-2.discordapp.net/external/9Q_b70cs1yvSzxGzU0wXo2QWUDMr12uRi5FUIybQcNQ/%3Fv%3D1/https/cdn.discordapp.com/emojis/695541716760920086.gif')   
     .setTimestamp()
