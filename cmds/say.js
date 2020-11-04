5cfe0b8b63416c7b64e2aa7b21f27e6210814f00
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-04 14:51:17 +0600

    Update say.js

--- cmds/say.js
+++ cmds/say.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(twarn)
     let twarn = new Discord.RichEmbed()
     .setAuthor(`Ошибка`,'https://images-ext-2.discordapp.net/external/9Q_b70cs1yvSzxGzU0wXo2QWUDMr12uRi5FUIybQcNQ/%3Fv%3D1/https/cdn.discordapp.com/emojis/695541716760920086.gif')   
     .setTimestamp()
     .setColor('#00ff14')
     .setTitle('Недостаточно прав!')
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(twarn)
     let tfwarn = new Discord.RichEmbed()
     .setAuthor(`Ошибка`,'https://images-ext-2.discordapp.net/external/9Q_b70cs1yvSzxGzU0wXo2QWUDMr12uRi5FUIybQcNQ/%3Fv%3D1/https/cdn.discordapp.com/emojis/695541716760920086.gif')   
     .setTimestamp()
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
     let result = new Discord.RichEmbed()
     .setColor('#00ff14')
     .setTitle(`${content}`)
-    if (!content) return message.reply(tfwarn)
+    if (!content) return message.channel.send(tfwarn)
     await message.delete()
     await message.channel.send(result)
 }
