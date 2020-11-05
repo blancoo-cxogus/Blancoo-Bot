969e4dba02c68e48dbf41634bbc40c75c99e0fc0
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-05 11:40:51 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,32 @@ fs.readdir('./cmds/',(err,files)=>{
         bot.commands.set(props.help.name,props);
     });
 });
+
+bot.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.channel.type == "dm") return;
+    let uid = message.author.id;
+    bot.send = function (msg){
+        message.channel.send(msg);
+    };
+    if(!profile[uid]){
+        profile[uid] ={
+            coins:10,
+            warns:0,
+            xp:0,
+            lvl:1,
+        };
+    };
+    let u = profile[uid];
+
+    u.coins++;
+    u.xp++;
+
+    if(u.xp>= (u.lvl * 5)){
+        u.xp = 0;
+        u.lvl += 1;
+    };
+    
 bot.on('ready', () => {
 let status = [',help', ',help']
 let status_res = Math.floor(Math.random() * status.length)
@@ -48,6 +74,15 @@ bot.user.setActivity(status[status_res], {type: "Watching"}) }, 10000)
     },5000)
 
 });
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0].toLowerCase();
+    let args = messageArray.slice(1);
+    if(!message.content.startsWith(prefix)) return;
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if(cmd) cmd.run(bot,message,args);
+    bot.rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    bot.uId = message.author.id;
+
     fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
         if(err) console.log(err);
     });
