74493c77e43a6c622ce8da1d3710d715a91239c3
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-05 11:38:10 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,14 +18,11 @@ fs.readdir('./cmds/',(err,files)=>{
         bot.commands.set(props.help.name,props);
     });
 });
-
-
 bot.on('ready', () => {
 let status = [',help', ',help']
 let status_res = Math.floor(Math.random() * status.length)
 setInterval(() => {
 bot.user.setActivity(status[status_res], {type: "Watching"}) }, 10000)
-
     console.log(`Запустился бот ${bot.user.username}`);
     bot.generateInvite(["ADMINISTRATOR"]).then(link =>{
         console.log(link);
@@ -51,38 +48,7 @@ bot.user.setActivity(status[status_res], {type: "Watching"}) }, 10000)
     },5000)
 
 });
-
-const configa = {
-  voice: "724901156148740190",
-  parent: "724901156148740187"
-}
-//Вместо инстансов GuildMember, используются инстансы VoiceState, что равносильно member.voice
-bot.on("voiceStateUpdate", (oldState, newState) => {
-  if(newState.channelID === configa.voice) {
-    newState.guild.channels.create("Имя привата", {
-      type: "VOICE",
-      parent: configa.parent,
-      permissionOverwrites: [
-        {
-           id: newState.guild.id, //Права для роли @everyone
-           allow: ["VIEW_CHANNEL"]
-        },
-        {
-          id: newState.member.id, //Права для создателя канала
-          allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"]
-        }
-      ]
-    }).then(ch => newState.setChannel(ch))
-  }
-  //удаление канала, если в нем больше не осталось человек
-  if(oldState.channel && !oldState.channel.members.size && oldState.channel.parentID === configa.parent && oldState.channelID !== configa.voice) oldState.channel.delete();
-})
-
-
-
     fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
         if(err) console.log(err);
     });
-
-
 bot.login(process.env.BOT_TOKEN)
