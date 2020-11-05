d9dec32d9217af5da7d6f55d2ded6794608843d7
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-05 12:40:05 +0600

    Update best.js

--- cmds/best.js
+++ cmds/best.js
@@ -1,4 +1,4 @@
-onst Discord = require("discord.js");
+const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 function randomIntInc(low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
