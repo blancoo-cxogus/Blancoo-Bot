394f52292ad2200f1441986bc9b5b10cd7e91dcb
TcXogus <59112407+TcXogus@users.noreply.github.com>
2020-11-06 13:21:49 +0600

    Update tableflip.js

--- cmds/tableflip.js
+++ cmds/tableflip.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js");
-
 const bot = new Discord.Client();
-
 const frames = [
-
 '(-°□°)- ┬─┬',
 
 '(╯°□°)╯ ]',
@@ -15,26 +12,15 @@ const frames = [
 '(╯°□°)╯ ┬─┬'
 
 ];
-
 module.exports.run = async(bot, message, args) => {
-
 message.delete()
-
 const msg = await message.channel.send('(\\\\°□°)\\\\ ┬─┬');
-
 for (const frame of frames) {
-
 setTimeout(() => {}, 4000);
-
 await msg.edit(frame);
-
 }
-
 return message;
-
 }
-
 module.exports.help= {
-
 name: "tableflip"
 } 
